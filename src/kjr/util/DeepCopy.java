beb05ebfbb81557a5fa1626ee9c8ce1360cfe7c9
definestructunion <matthewblaschek@gmail.com>
2019-03-08 14:15:10 -0800

    Added Javadoc to DeepCopy and ShallowCopy

--- src/kjr/util/DeepCopy.java
+++ src/kjr/util/DeepCopy.java
@@ -1,5 +1,15 @@
 package kjr.util;
 
+/**
+ * Allows user implemention to deep copy all references as new values. to implement, ensure that
+ * the class implementing DeepCopy copies the class implementing it. For Example:
+ * <pre>
+ * public class Foo implements DeepCopy<Foo>
+ * </pre>
+ * After declaring that the class implements DeepCopy<T>, override copy() to copy all reference
+ * objects.
+ * @param <T> the type of the class to copy.
+ */
 public interface DeepCopy<T>
 {
     public T copy();
--- src/kjr/util/ShallowCopy.java
+++ src/kjr/util/ShallowCopy.java
@@ -1,5 +1,15 @@
 package kjr.util;
 
+/**
+ * Allows user implemention to shallow copy an object and keep all the references the same. to implement, ensure that
+ * the class implementing ShallowCopy copies the class implementing it. For Example:
+ * <pre>
+ * public class Foo implements ShallowCopy<Foo>
+ * </pre>
+ * After declaring that the class implements ShallowCopy<T>, override copy() to keep all reference
+ * objects the same.
+ * @param <T> the type of the class to copy.
+ */
 public interface ShallowCopy<T>
 {
     public T copy();
